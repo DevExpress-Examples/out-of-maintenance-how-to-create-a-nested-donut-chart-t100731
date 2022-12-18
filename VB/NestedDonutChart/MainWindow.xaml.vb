f5cd45142b9989dca7ca2d9d91af5aef5921add5
DevExpressExampleBot <fakeEmail>
2022-12-18 17:25:03 +0400

    Source auto update [skip ci]

--- VB/NestedDonutChart/MainWindow.xaml.vb
+++ VB/NestedDonutChart/MainWindow.xaml.vb
@@ -1,29 +1,24 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Xpf.Charts
 Imports System.Windows.Media
 
 Namespace NestedDonutChart
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub chartControl1_CustomDrawSeriesPoint(ByVal sender As Object, ByVal e As CustomDrawSeriesPointEventArgs)
-
-			If e.SeriesPoint.Value <= 10 Then
-				e.DrawOptions.Color = Color.FromArgb(&HFF, &H51, &H89, &H3)
-
-			ElseIf (e.SeriesPoint.Value > 10) And (e.SeriesPoint.Value <= 20) Then
-				e.DrawOptions.Color = Color.FromArgb(&HFF, &HF9, &HAA, &HF)
-
-			ElseIf e.SeriesPoint.Value > 20 Then
-				e.DrawOptions.Color = Color.FromArgb(&HFF, &HC7, &H39, &HC)
-			End If
-		End Sub
-	End Class
+        Private Sub chartControl1_CustomDrawSeriesPoint(ByVal sender As Object, ByVal e As CustomDrawSeriesPointEventArgs)
+            If e.SeriesPoint.Value <= 10 Then
+                e.DrawOptions.Color = Color.FromArgb(&HFF, &H51, &H89, &H03)
+            ElseIf e.SeriesPoint.Value > 10 And e.SeriesPoint.Value <= 20 Then
+                e.DrawOptions.Color = Color.FromArgb(&HFF, &HF9, &HAA, &H0F)
+            ElseIf e.SeriesPoint.Value > 20 Then
+                e.DrawOptions.Color = Color.FromArgb(&HFF, &HC7, &H39, &H0C)
+            End If
+        End Sub
+    End Class
 End Namespace
-
-
